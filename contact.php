e2381dea967c9cb69bfdbcd7428ac27a89ee8bac
hajerlayas <46102907+hajerlayas@users.noreply.github.com>
2019-01-18 01:07:35 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -6,27 +6,7 @@ include "nav.php";
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <title>One Page Bootstrap Template</title>
 
-  <!-- Bootstrap -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="css/font-awesome.min.css">
-  <link href="css/animate.min.css" rel="stylesheet">
-  <link href="css/animate.css" rel="stylesheet" />
-  <link href="css/prettyPhoto.css" rel="stylesheet">
-  <link href="css/style.css" rel="stylesheet">
-  <link href="css/contactus.css" rel="stylesheet">
-  <!-- =======================================================
-    Theme Name: OnePage
-    Theme URL: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/
-    Author: BootstrapMade
-    Author URL: https://bootstrapmade.com
-  ======================================================= -->
 </head>
 
 <body>
