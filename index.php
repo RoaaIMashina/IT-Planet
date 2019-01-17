8ddf98f465b90c1fa4a38f933d0e98ce6ee05a62
RoaaIMashina <45904723+RoaaIMashina@users.noreply.github.com>
2019-01-18 00:34:38 +0200

    Update index.php
    accordion

--- index.php
+++ index.php
@@ -11,16 +11,12 @@ include 'nav.php';
 <body>
  
 
-  <div id="home">
-   
-     
-  </div>
-
+<!--#accordion-->
   <section id="about">
     <div class="container">
       <div class="center">
         <div class="col-md-6 col-md-offset-3">
-          <h2>About Us</h2>
+          <h2>the most shosen courses</h2>
           <hr>
           <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut</p>
         </div>
@@ -39,90 +35,53 @@ include 'nav.php';
         <div class="col-sm-6 wow fadeInDown">
           <div class="accordion">
             <div class="panel-group" id="accordion1">
+             
+             
               <div class="panel panel-default">
-                <div class="panel-heading active">
-                  <h3 class="panel-title">
-                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapseOne1">
-                      Web Design
-                      <i class="fa fa-angle-right pull-right"></i>
-                    </a>
-                  </h3>
-                </div>
-
-                <div id="collapseOne1" class="panel-collapse collapse in">
-                  <div class="panel-body">
-                    <div class="media accordion-inner">
-                      <div class="pull-left">
-                        <img class="img-responsive" src="images/accordion1.png">
-                      </div>
-                      <div class="media-body">
-                        <h4>Adipisicing elit</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-                          ut labore</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
+                <div class="acc">
+    <div class="acc-titel" id="acc1">CSS</div>
+    <div class="acc-content">
+      <h2 id="h">Cascading Style Sheets</h2>
+      <p id="p">CSS is a language that describes the style of an HTML document.
+CSS describes how HTML elements should be displayed.
+This tutorial will teach you CSS from basic to advanced.</p>
+    </div>
+  </div>
+     </div>
+             
               <div class="panel panel-default">
-                <div class="panel-heading">
-                  <h3 class="panel-title">
-                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapseTwo1">
-                      Lorem ipsum dolor sit amet
-                      <i class="fa fa-angle-right pull-right"></i>
-                    </a>
-                  </h3>
-                </div>
-                <div id="collapseTwo1" class="panel-collapse collapse">
-                  <div class="panel-body">
-                    <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                      3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-                      laborum eiusmod. Brunch 3 wolf moon tempor.<br> Lorem ipsum dolor sit amet, consectetur
-                      adipisicing elit, sed do eiusmod tempor incididunt ut non cupidatat skateboard dolor brunch.</p>
-                  </div>
-                </div>
-              </div>
+                           <div class="acc">
+    <div class="acc-titel" id="acc2">JAVA</div>
+    <div class="acc-content">
+      <h2 id="h">JAVA</h2>
+      <p id="p">Java is a high-level programming language originally developed by Sun Microsystems and released in 1995. Java runs on a variety of platforms, such as Windows, Mac OS, and the various versions of UNIX. This tutorial gives a complete understanding of Java. This reference will take you through simple and practical approaches while learning Java Programming language</p>
+    </div>
+      </div>      
+        </div>
+             
+            <div class="panel panel-default">
+                 <div class="acc">
+    <div class="acc-titel" id="acc3">C#</div>
+    <div class="acc-content">
+      <h2 id="h">C Sharp</h2>
+      <p id="p">C# programming is very much based on C and C++ programming languages, so if you have a basic understanding of C or C++ programming, then it will be fun to learn C#.</p>
+    </div>
+  </div>
+     </div>
 
+             
               <div class="panel panel-default">
-                <div class="panel-heading">
-                  <h3 class="panel-title">
-                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapseThree1">
-                      Lorem ipsum dolor sit amet
-                      <i class="fa fa-angle-right pull-right"></i>
-                    </a>
-                  </h3>
-                </div>
-                <div id="collapseThree1" class="panel-collapse collapse">
-                  <div class="panel-body">
-                    <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                      3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-                      laborum eiusmod. Brunch 3 wolf moon tempor.<br> Lorem ipsum dolor sit amet, consectetur
-                      adipisicing elit, sed do eiusmod tempor incididunt ut non cupidatat skateboard dolor brunch.</p>
-                  </div>
-                </div>
-              </div>
+                 <div class="acc">
+    <div class="acc-titel" id="acc4">MicroSoft Word</div>
+    <div class="acc-content">
+      <h2 id="h">MicroSoft Word</h2>
+      <p id="p">Microsoft Office Word allows you to create and edit personal and business documents, such as letters, reports, invoices, emails and books. By default, documents saved in Word  are saved with the .docx extension. </p>
+    </div>
+  </div>
+     </div>
 
-              <div class="panel panel-default">
-                <div class="panel-heading">
-                  <h3 class="panel-title">
-                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapseFour1">
-                      Lorem ipsum dolor sit amet
-                      <i class="fa fa-angle-right pull-right"></i>
-                    </a>
-                  </h3>
-                </div>
-                <div id="collapseFour1" class="panel-collapse collapse">
-                  <div class="panel-body">
-                    <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                      3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-                      laborum eiusmod. Brunch 3 wolf moon tempor.<br> Lorem ipsum dolor sit amet, consectetur
-                      adipisicing elit, sed do eiusmod tempor incididunt ut non cupidatat skateboard dolor brunch.</p>
-                  </div>
-                </div>
-              </div>
             </div>
-            <!--/#accordion1-->
+            
           </div>
         </div>
 
@@ -131,7 +90,7 @@ include 'nav.php';
     </div>
     <!--/.container-->
   </section>
-  <!--/#about-->
+  <!--/#accordion-->
 
   <section id="portfolio">
     <div class="container">
