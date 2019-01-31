6e0f7c17fb12aff8994130d4034c8e2221989576
ayalamokhzanji <46103758+ayalamokhzanji@users.noreply.github.com>
2019-01-31 18:53:09 +0200

    Add files via upload
    footer "يخدم بالنت "

---
+++ footer.php
@@ -0,0 +1,53 @@
+
+ <!DOCTYPE html>
+ 
+<html lang="en">
+
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title></title>
+
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link href="css/animate.min.css" rel="stylesheet">
+  <link href="css/animate.css" rel="stylesheet" />
+  <link href="css/prettyPhoto.css" rel="stylesheet">
+
+  <link href="css/contactus.css" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <link href="css/style1.css" rel="stylesheet"></head>
+</head>
+
+<body>
+
+  <footer id="footer" class="midnight-blue">
+
+            <a href="#top" class="scrollup"><i class="fa fa-angle-up fa-3x"></i></a>
+   
+          <div class="text">
+          &copy; OnePage Theme. All Rights Reserved.<br>
+        
+        this website was made as an assignment for advanced internet programming "ITSE412" -<br> factulty of information technology - University of Tripoli
+           
+		
+            <ul >
+                <li><a id="f" href="https://www.facebook.com/ituot/"><i class="fa fa-facebook" title="facebook"></i></a></li>
+                <li><a  id="f" href="https://github.com/RoaaIMashina/IT-PLANET"><i class="fa fa-github "title="GitHub"></i></a></li>
+                <li><a id="f" href="http://uot.edu.ly/"><i class="fa fa-building"title="website"></i></a></li>
+                <li><a id="f" href="http://it.uot.edu.ly/"><i class="fa fa-building"title="website"></i></a></li>             
+			 </ul>
+            </div>
+         
+  
+      </div>
+    </div>
+  </footer>
+  </body>
+</html>
