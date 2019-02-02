8e6f6e2ba79407154301583b2eda9d5cd429baab
OlaKhoja <46102829+OlaKhoja@users.noreply.github.com>
2019-02-02 18:36:55 +0200

    Add files via upload

---
+++ db.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="ITPLANET";
+//create connection
+$conn= new mysqli($servername,$username,$password,$dbname);
+//check connection
+if($conn->connect_error){
+ die("connection failed:".$conn->connect_error);
+ 
+}
+</body>
+</html>
