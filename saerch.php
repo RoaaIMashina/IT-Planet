a3430ad07aa89c6bf7f03e7cd01efb4d227b27d5
hajerlayas <46102907+hajerlayas@users.noreply.github.com>
2019-02-04 18:03:16 +0200

    Update saerch.php

--- saerch.php
+++ saerch.php
@@ -17,33 +17,7 @@ $(document).ready(function(){
 });
 
 </script>
-<style>
 
-.closed {
-  position: absolute;
-  top:80px;
- left:480px;
-  color: #00BCD4;
-  font-size: 20px;
-  font-weight: bold;
-  transition: 0.3s;
-}
-
-.closed:hover,
-.closed:focus {
-  color: #117A65;
-  text-decoration: none;
-  cursor: pointer;
-}
-.input-group{
-	 margin-left:-160px;
-}
-#result {
-		 margin-left:-1000px;
-	
-	
-}
-</style>
 </head>
 <body>
 
@@ -53,7 +27,6 @@ $(document).ready(function(){
      <span class="input-group-addon"  style="  background-color:#a6c1ee;"  >Search</span>
      <input type="text" name="search_text" id="search_text" placeholder="Search by Customer Details" class="form-control" style="width:30%" />
     </div>
-<span class="closed" onclick="closeModal()">&times;</span>
 
    </div>
    <br />
