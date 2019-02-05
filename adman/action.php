70ae99834c59aa45f7a24eeb32f66217fd4149a6
ayalamokhzanji <46103758+ayalamokhzanji@users.noreply.github.com>
2019-02-05 20:24:19 +0200

    Update action.php
    Delete courses

--- adman/action.php
+++ adman/action.php
@@ -32,7 +32,22 @@ include 'CursessTable.php';
            ";  
            $object->execute_query($query);  
            echo 'Data Inserted';  
-      }  
+      } 
+  
+  //aya.almokhzanji@gmail.com
+  //delet from table
+   if($_POST["action"] == "Delete")  
+      {    
+  $id =mysqli_real_escape_string($object->connect, $_POST["id"]);  
+    $checkbox = $_POST['id'];
+for($i=0;$i<count($checkbox);$i++){
+$del_id = $checkbox[$i];
+$sql2 = "DELETE FROM lecture WHERE id_L =' $del_id'";
+$object->execute_query($sql2);  
+           echo 'Data Deleted'; 
+}}
+    
+            
 
 }
  
