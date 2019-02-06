dc45f8d387938c95429b92993dbc6c8cd16abf26
RoaaIMashina <45904723+RoaaIMashina@users.noreply.github.com>
2019-02-06 11:31:18 +0200

    Create admin.php

---
+++ adman/admin.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+ <div id="a" style=" display: none; 
+  position: fixed; 
+  z-index: 1; 
+  left: 0;
+  top: 0;
+  width: 100%; 
+  height: 100%; 
+  background-color: black; 
+  background-color: rgba(0,0,0,0.69); ">
+  
+    <section style=" width: 26%;
+    margin-bottom: 106px;
+    margin-top: 177px;
+    padding: 50px;
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;">
+    
+<div style="padding-top:47px;
+  padding-bottom: 21px;
+  background:#eee;
+  margin:auto;
+  border:1px solid black;    
+  margin-top: 143px;
+  border-radius: 49px;
+  background-color:#cfeaf3;">
+  <p style="
+    color: #053d6a;
+    text-align: center;
+    font-size: 30px;
+    margin-top: -34px;">Adman settings</p>
+<ul style="list-style-type: none;
+    margin-left: -43px;">
+  <li><a href="addcourse.php">Add Cursess</a></li>
+<li><a href="deletcourses.php">Delete Cursess</a></li>
+<li><a href="user.php">user Details</a></li>
+</ul>
+
+</div>
+
+
+  </section>
+</div>
+</body>
+</html>
